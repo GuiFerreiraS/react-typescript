55492754581810cc3c56be29497f4551704e2084
jooaodias <joaovitordias92040@gmail.com>
2022-02-14 21:11:22 -0300

    Iniciando setup

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+const StyledContainer = styled.div`
+  height: 100%;
+  color: #fca71c;
+  font-family: sans-serif;
+`;
+const Home = () => {
+  return (
+    <StyledContainer>
+      <h3>Landing page project 🦾</h3>
+    </StyledContainer>
+  );
+};
+
+export default Home;
